cb13fe7351ae4e50ca05a339e4203181cecc0a48
marvieiradev <marcosvieira482@gmail.com>
2025-02-11 23:37:35 -0300

    add RSI function

--- index.js
+++ index.js
@@ -1,28 +1,64 @@
 const axios = require("axios");
 
 const SYMBOL = "BTCUSDT";
-const BUY_PRICE = 34160;
-const SELL_PRICE = 34501;
+const PERIOD = 14;
 
-const API_URL = "https://testnet.binance.vision";
-//https://api.binance.com
+const API_URL = "https://api.binance.com"; //"https://testnet.binance.vision";
+
+function averages(prices, period, startIndex) {
+  let gains = 0,
+    losses = 0;
+
+  for (let i = 0; i < period && i + startIndex < prices.length; i++) {
+    const diff = prices[i + startIndex] - prices[i + startIndex - 1];
+    if (diff >= 0) gains += diff;
+    else losses += Math.abs(diff);
+  }
+  let avgGains = gains / period;
+  let avgLosses = losses / period;
+  return { avgGains, avgLosses };
+}
+
+function RSI(prices, period) {
+  let avgGains = 0,
+    avgLosses = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    let newAverages = averages(prices, period, i);
+
+    if (i === 1) {
+      avgGains = newAverages.avgGains;
+      avgLosses = newAverages.avgLosses;
+      continue;
+    }
+
+    avgGains = (avgGains * (period - 1) + newAverages.avgGains) / period;
+    avgLosses = (avgLosses * (period - 1) + newAverages.avgLosses) / period;
+  }
+  const rs = avgGains / avgLosses;
+  return 100 - 100 / (1 + rs);
+}
 
 let isOpened = false;
 
 async function start() {
   const { data } = await axios.get(
-    API_URL + "/api/v3/klines?limit=21&interval=15m&symbol=" + SYMBOL
+    API_URL + "/api/v3/klines?limit=100&interval=15m&symbol=" + SYMBOL
   );
   const candle = data[data.length - 1];
-  const price = parseFloat(candle[4]);
+  const lastPrice = parseFloat(candle[4]);
   console.clear();
-  console.log("Preço:" + price);
+  console.log("Preço:" + lastPrice);
 
-  if (price <= BUY_PRICE && isOpened === false) {
-    console.log("COMPRAR");
+  const prices = data.map((k) => parseFloat(k[4]));
+  const rsi = RSI(prices, PERIOD);
+  console.log("RSI: " + rsi);
+
+  if (rsi < 30 && isOpened === false) {
+    console.log("SOBREVENDIDO, HORA DE COMPRAR");
     isOpened = true;
-  } else if (price >= SELL_PRICE && isOpened === true) {
-    console.log("VENDER");
+  } else if (rsi > 70 && isOpened === true) {
+    console.log("SOBRECOMPRADO, HORA DE VENDER");
     isOpened = false;
   } else {
     console.log("AGUARDAR...");
