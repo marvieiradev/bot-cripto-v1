3bc41fedb51967b4249118ecde068e2a1b77238e
marvieiradev <marcosvieira482@gmail.com>
2025-02-12 19:42:53 -0300

    added function to automatically buy and sell

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const axios = require("axios");
+const crypto = require("crypto");
 
 const SYMBOL = "BTCUSDT";
+const QUANTITY = "0.001";
 const PERIOD = 14;
 
-const API_URL = "https://api.binance.com"; //"https://testnet.binance.vision";
+const API_URL = "https://testnet.binance.vision"; //API_URL de teste.
+//Essa é a API_URL de produção: "https://api.binance.com";
+//A API_KEY e SECRET_KEY devem ser criadas em: https://testnet.binance.vision.
+const API_KEY = "sua_api_key";
+const SECRET_KEY = "sua_secret_key";
 
 function averages(prices, period, startIndex) {
   let gains = 0,
@@ -39,6 +45,30 @@ function RSI(prices, period) {
   return 100 - 100 / (1 + rs);
 }
 
+async function newOrder(symbol, quantity, side) {
+  const order = { symbol, quantity, side };
+  order.type = "MARKET";
+  order.timestamp = Date.now();
+
+  const signature = crypto
+    .createHmac("sha256", SECRET_KEY)
+    .update(new URLSearchParams(order).toString())
+    .digest("hex");
+
+  order.signature = signature;
+
+  try {
+    const { data } = await axios.post(
+      API_URL + "/api/v3/order",
+      new URLSearchParams(order).toString(),
+      { headers: { "X-MBX-APIKEY": API_KEY } }
+    );
+    console.log(data);
+  } catch (error) {
+    console.error(error.response.data);
+  }
+}
+
 let isOpened = false;
 
 async function start() {
@@ -53,15 +83,18 @@ async function start() {
   const prices = data.map((k) => parseFloat(k[4]));
   const rsi = RSI(prices, PERIOD);
   console.log("RSI: " + rsi);
+  console.log("Já comprei? " + (isOpened === true ? "Sim" : "Não") + "\n");
 
   if (rsi < 30 && isOpened === false) {
-    console.log("SOBREVENDIDO, HORA DE COMPRAR");
+    console.log("Hora de comprar!");
     isOpened = true;
+    newOrder(SYMBOL, QUANTITY, "BUY");
   } else if (rsi > 70 && isOpened === true) {
-    console.log("SOBRECOMPRADO, HORA DE VENDER");
+    console.log("Hora de vender!");
+    newOrder(SYMBOL, QUANTITY, "SELL");
     isOpened = false;
   } else {
-    console.log("AGUARDAR...");
+    console.log("Aguardar...");
   }
 }
 
